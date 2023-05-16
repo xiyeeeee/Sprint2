578bfba757018191ea3aef3fe61581f6fa67d79a
xiyeeeee <claudiachenxiye@gmail.com>
2023-05-16 17:06:48 +0800

    clientbooking styling

--- MSPcode/clientbooking.php
+++ MSPcode/clientbooking.php
@@ -8,7 +8,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
         <link rel="shortcut icon" href="img/fav-icon.jpg" type="image/jpg">
         <link rel="stylesheet" type="text/css" href="css/style.css">
-
+        <link rel="stylesheet" type="text/css" href="css/style5.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
         <title>BOOKING: ETMP</title>
     </head>
